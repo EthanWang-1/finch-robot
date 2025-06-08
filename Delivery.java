ff8004449521e5d8f65820751664263f72e4d4a8
EthanWang-1 <ethanw97@nycstudents.net>
2025-06-07 23:28:54 -0400

    Update Delivery.java

--- Delivery.java
+++ Delivery.java
@@ -1,1 +1,21 @@
+package FinchPackage;
 
+public class follow {
+	public static void main(String[] args) {
+		Finch f1 = new Finch();
+		
+		while (true) {
+			int dist = f1.getDistance();
+			if(dist>10) {
+				f1.setMotors(10,10);
+			}
+			f1.stop();
+			
+			if((int)Math.random()*2 == 1){
+				f1.turn("R",90,10);
+			}else {
+				f1.turn("R",90,10);	
+			}
+			f1.stop();
+		}
+	}
